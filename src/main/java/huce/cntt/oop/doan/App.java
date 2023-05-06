314371e9804d07e2a7359db87266547ea21b94fe
duyhelloworld <codedaovoiduy@gmail.com>
2023-05-06 12:38:41 +0700

    thêm fxml, tính xử lí 1 chút tương tác nút

--- src/main/java/huce/cntt/oop/doan/App.java
+++ src/main/java/huce/cntt/oop/doan/App.java
@@ -3,61 +3,34 @@ package huce.cntt.oop.doan;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     
-    private Scene home, login;
-    private Stage windowHello, primaryStage;
-
-    // private Scene home(){
-    //     StackPane pane = new StackPane();
-    //     Label welcome = new Label("Welcome !");
-    //     pane.getChildren().add(welcome);
-
-    //     home = new Scene(pane, 400, 500);
-    //     return home;
-    // }
-
-    private Scene login() {
-        StackPane pane = new StackPane();
-        Button loginBtn = new Button("Login Now");
-        loginBtn.setOnAction(e -> {
-            System.out.println("Logined in");
-            primaryStage.setScene(home);
-        });
-        Label loginLabel = new Label("Login Page");
-        pane.getChildren().addAll(loginLabel, loginBtn);
-
-        login = new Scene(pane, 400, 500);
-        return login;
-    }   
+    private Scene bangQuanLiMonHoc, bangQuanLiMonHoc_Loc;
+    private Stage stageChinh, stagePhu;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        windowHello.setTitle("HUCE student management system");
-        windowHello.setScene(login());
-        
-        primaryStage = windowHello;
+        // primaryStage = stageChinh;
+
+        // String fxmlPath = "/huce/cntt/oop/doan/bang_quanli_monhoc_locTim.fxml";
+        String fxmlPath = "/huce/cntt/oop/doan/profile_sinhvien.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+
+        Pane root = loader.load();
+        // bangQuanLiMonHoc = new Scene(root);
+        // primaryStage.setScene(bangQuanLiMonHoc);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("HUCE student management system");
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
-        // DiaChi dc1 = new DiaChi("Trung Luong, Binh Luc, Ha Nam");
-        // DiaChi dc2 = new DiaChi("9, Binh Luc, Ha Nam");
-        // DiaChi dc3 = new DiaChi("9,1 , Binh Luc, Ha Nam");
-        // DiaChi dc4 = new DiaChi("3,5 , Binh Luc, Ha Nam");
-
-        // QuanLiSinhVienTheoLop ql = new QuanLiSinhVienTheoLop();
-        // // List<Map<St\ring, String>> kq = ql.laySinhVienTheoLopQuanLi("66IT5");
-        // List<Map<String, String>> kq = ql.laySinhVienTheoLopMonHoc("66ML1");
-        // for (Map<String,String> sinhVien : kq) {
-        // System.out.println(sinhVien.toString());
-        // }
 }
---
+++ src/main/java/huce/cntt/oop/doan/controller/QuanLiMH_Ctl.java
@@ -0,0 +1,14 @@
+package huce.cntt.oop.doan.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class QuanLiMH_Ctl {
+    @FXML
+    private Button nutLoc;
+
+    @FXML
+    private void clickSave(){
+        System.out.println("Clicked");
+    }
+}
