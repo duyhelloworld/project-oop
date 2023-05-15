85b44ca2adb5c8adb1ed43d9841f68586bf388f8
duyhelloworld <codedaovoiduy@gmail.com>
2023-05-15 23:37:43 +0700

    hàm insert

--- src/main/java/huce/cntt/oop/doan/service/SinhVienService.java
+++ src/main/java/huce/cntt/oop/doan/service/SinhVienService.java
@@ -43,28 +43,31 @@ public class SinhVienService implements ISinhVienService {
     }
 
     @Override
-    public SinhVien laySinhVienTheoMaSo(Integer maso){
-        SinhVien kq = new SinhVien();
+    public SinhVien timKiemSinhVienTheoMaSo(Integer maso){
+        SinhVien sv = new SinhVien();
         PreparedStatement statement = access.getStatement("SELECT * FROM SinhVien WHERE mssv = ?");
         
         try {
             statement.setInt(1, maso);
             ResultSet result = statement.executeQuery();
             while (result.next()) {
-                kq.setMaso(maso);
-                kq.setHoTen(new HoTen(result.getString("ho_ten")));
-                kq.setGioiTinh(result.getBoolean("gioi_tinh"));
-                kq.setNgaySinh(
+                sv.setMaso(maso);
+                sv.setHoTen(new HoTen(result.getString("ho_ten")));
+                sv.setGioiTinh(result.getBoolean("gioi_tinh"));
+                sv.setNgaySinh(
                         result.getDate("ngay_sinh").toLocalDate());
-                kq.setQueQuan(new DiaChi(result.getString("que_quan")));
-                kq.setDiaChiHienTai(new DiaChi(result.getString("dia_chi_hien_tai")));
-                kq.setSoDienThoai(result.getString("so_dien_thoai"));
+                sv.setQueQuan(new DiaChi(result.getString("que_quan")));
+                sv.setDiaChiHienTai(new DiaChi(result.getString("dia_chi_hien_tai")));
+                sv.setSoDienThoai(result.getString("so_dien_thoai"));
+                sv.setEmail(result.getString("email"));
             }
             access.closeConnection(statement);
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (IllegalArgumentException e){
+            System.out.println("Exception :" + e.getMessage());
         }
-        return kq;
+        return sv;
     }
 
     @Override
@@ -86,31 +89,58 @@ public class SinhVienService implements ISinhVienService {
                 temp.setDiaChiHienTai(new DiaChi(result.getString("dia_chi_hien_tai")));
                 temp.setSoDienThoai(result.getString("so_dien_thoai"));
                 kq.add(temp);
+                temp = null;
             }
             access.closeConnection(statement);
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        } 
         return kq;
     }
 
     @Override
-    public void themMoiSinhVien(SinhVien sinhVien){
-        if (sinhVien.getMaso() != null) {
-            throw new IllegalArgumentException("property 'mssv' is not null");
-        }
-        PreparedStatement statement = access.getStatement("INSERT INTO SInhVien VALUE (DEFAULT, ?, ?, ?, ?)");
+    public Integer themMoiSinhVien(SinhVien sinhVien) {
+        int maSoSinhVien = 0;
         try {
-            // statement;
+            if (sinhVien.getMaso() != null) {
+                throw new IllegalArgumentException("'mssv' must be NULL");
+            }
+            PreparedStatement statement = access.getStatement("INSERT INTO SinhVien (ho_ten, gioi_tinh, ngay_sinh, dia_chi_hien_tai, que_quan, email, so_dien_thoai, ngay_vao_truong) VALUE (?, ?, ?, ?, ?, ?, ?, ?)");
+            statement.setString(1, sinhVien.getHoTen().toString());
+            statement.setBoolean(2, sinhVien.getGioiTinh());
+            statement.setDate(3, sinhVien.getNgaySinhVoiKieuSQL());
+            statement.setString(4, sinhVien.getDiaChiHienTai().formatToSaveDataBase());
+            statement.setString(5, sinhVien.getQueQuan().formatToSaveDataBase());
+            statement.setString(6, sinhVien.getEmail());
+            statement.setString(7, sinhVien.getSoDienThoai());
+            statement.setDate(8, sinhVien.getNgayVaoTruongVoiKieuSQL());
+
+            int soDongAnhHuong = statement.executeUpdate();
+
+            if (soDongAnhHuong == 1) {
+                System.out.println("inserted successfully!");
+                ResultSet resultSet = statement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    maSoSinhVien = resultSet.getInt(1);
+                }
+            } else {
+                throw new IllegalStateException("This 'sinhvien' isn't inserted !");
+            }
             statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (IllegalArgumentException e){
+            System.out.println("Exception :" + e.getMessage());
+        } catch (IllegalStateException e) {
+            System.out.println("We meet some problem when inserting!");
+            System.out.println("Exception :" + e.getMessage());
         }
+        return maSoSinhVien;
     }
 
     @Override
-    public Boolean capNhatThongTinSinhVien(Integer mssv, SinhVien sinhVienTruyenVao) {
-        SinhVien sinhVienLayRaTuDatabase = laySinhVienTheoMaSo(mssv);
+    public void capNhatThongTinSinhVien(Integer mssv, SinhVien sinhVienTruyenVao) {
+        SinhVien sinhVienLayRaTuDatabase = timKiemSinhVienTheoMaSo(mssv);
         
         try {
             if (sinhVienLayRaTuDatabase == null) {
@@ -118,49 +148,90 @@ public class SinhVienService implements ISinhVienService {
             }
 
             PreparedStatement statement = access.getStatement("UPDATE SinhVien SET ho_ten = ?, ngay_sinh = ?, gioi_tinh = ?, que_quan = ?, dia_chi_hien_tai = ?, so_dien_thoai = ? WHERE mssv = ?");
-            if (sinhVienTruyenVao.getHoTen() != null) {
+            if (sinhVienTruyenVao.getHoTen() != sinhVienTruyenVao.getHoTen()) {
                 statement.setString(1, sinhVienTruyenVao.getHoTen().toString());
             } else {
                 statement.setString(1, sinhVienLayRaTuDatabase.getHoTen().toString());
             }
 
-            if (sinhVienTruyenVao.getNgaySinh() != null) {
+            if (sinhVienTruyenVao.getNgaySinh() != sinhVienTruyenVao.getNgaySinh()) {
                 statement.setDate(2, sinhVienTruyenVao.getNgaySinhVoiKieuSQL());
             } else {
                 statement.setDate(2, sinhVienLayRaTuDatabase.getNgaySinhVoiKieuSQL());
             }
 
-            if (sinhVienTruyenVao.getGioiTinh() != null) {
+            if (sinhVienTruyenVao.getGioiTinh() != sinhVienTruyenVao.getGioiTinh()) {
                 statement.setBoolean(3,  sinhVienTruyenVao.getGioiTinh());
             } else {
                 statement.setBoolean(3, sinhVienLayRaTuDatabase.getGioiTinh());
             }
 
-            if (sinhVienTruyenVao.getQueQuan() != null) {
+            if (sinhVienTruyenVao.getQueQuan() != sinhVienLayRaTuDatabase.getQueQuan()) {
                 statement.setString(4, sinhVienTruyenVao.getQueQuan().formatToSaveDataBase());
             } else {
                 statement.setString(4, sinhVienLayRaTuDatabase.getQueQuan().formatToSaveDataBase());
             }
 
-            if (sinhVienTruyenVao.getDiaChiHienTai() != null) {
+            if (sinhVienTruyenVao.getDiaChiHienTai() != sinhVienLayRaTuDatabase.getDiaChiHienTai()) {
                 statement.setString(5, sinhVienTruyenVao.getDiaChiHienTai().formatToSaveDataBase());
             } else {
                 statement.setString(5, sinhVienLayRaTuDatabase.getDiaChiHienTai().formatToSaveDataBase());
             }
 
-            if (sinhVienTruyenVao.getSoDienThoai()!= null) {
+            if (sinhVienTruyenVao.getSoDienThoai()!= sinhVienLayRaTuDatabase.getSoDienThoai()) {
                 statement.setString(6, sinhVienTruyenVao.getSoDienThoai());
             } else {
                 statement.setString(6, sinhVienLayRaTuDatabase.getSoDienThoai());
             }
             statement.setInt(7, mssv);
-            int rowAffected = statement.executeUpdate();
             access.closeConnection(statement);
-
-            return rowAffected == 1;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+    }
+
+    @Override
+    public SinhVien timKiemSinhVienTheoEmail(String email) {
+        SinhVien kq = new SinhVien();
+
+        if (!email.contains("@")) {
+            email = email.concat("@huce.edu.vn");
+        }
+
+        try {
+            PreparedStatement statement = access.getStatement("SELECT * FROM SinhVien WHERE email = ?");
+            statement.setString(1, email);
+            ResultSet result = statement.executeQuery();
+            if (result.next()) {
+                kq.setMaso(result.getInt("mssv"));
+                kq.setHoTen(new HoTen(result.getString("ho_ten")));
+                kq.setGioiTinh(result.getBoolean("gioi_tinh"));
+                kq.setNgaySinh(
+                    result.getDate("ngay_sinh").toLocalDate());
+                kq.setQueQuan(new DiaChi(result.getString("que_quan")));
+                kq.setDiaChiHienTai(new DiaChi(result.getString("dia_chi_hien_tai")));
+                kq.setSoDienThoai(result.getString("so_dien_thoai"));
+                kq.setEmail(email);
+            }
+            access.closeConnection(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        return kq;
+    }
+
+    @Override
+    public void xoaSinhVienTheoMaSo(List<Integer> mssvs) {
+        
+    }
+
+    @Override
+    public List<SinhVien> timSinhVienTheoLopQuanLi(Integer ma_lop_quan_li) {
+        throw new UnsupportedOperationException("Unimplemented method 'timSinhVienTheoLopQuanLi'");
+    }
+
+    @Override
+    public List<SinhVien> timSinhVienTheoLopMonHoc(Integer ma_lop_mon_hoc) {
+        throw new UnsupportedOperationException("Unimplemented method 'timSinhVienTheoLopMonHoc'");
     }
 }
