eaf8333d8bd6cb69e2e7e26f56858358f212c08d
duyhelloworld <codedaovoiduy@gmail.com>
2023-05-24 23:09:33 +0700

    dùng dto chứa thay

--- src/main/java/huce/cntt/oop/doan/entities/SinhVien.java
+++ src/main/java/huce/cntt/oop/doan/entities/SinhVien.java
@@ -2,19 +2,12 @@ package huce.cntt.oop.doan.entities;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.Objects;
 
 import huce.cntt.oop.doan.entities.parents.ConNguoi;
 
 public class SinhVien extends ConNguoi {
 
     private LocalDate ngayVaoTruong;
-
-    private String tenLopQuanLi;
-
-    private Integer nienKhoa;
-
-    private String khoa;
 
     public SinhVien() {
     }
@@ -31,48 +24,13 @@ public class SinhVien extends ConNguoi {
         this.ngayVaoTruong = ngayVaoTruong;
     }
 
-    public String getTenLopQuanLi() {
-        return this.tenLopQuanLi;
-    }
-
-    public void setTenLopQuanLi(String tenLopQuanLi) {
-        this.tenLopQuanLi = tenLopQuanLi;
-    }
-
-    public Integer getNienKhoa() {
-        if (ngayVaoTruong == null) {
-            return 0;
-        }
-        return this.ngayVaoTruong.getYear() - 1966;
-    }
-
-    public String getKhoa() {
-        return this.khoa;
-    }
-
-    public void setKhoa(String khoa) {
-        this.khoa = khoa;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof SinhVien)) {
-            return false;
-        }
-        SinhVien sinhVien = (SinhVien) o;
-        return super.equals(sinhVien) 
-            && Objects.equals(nienKhoa, sinhVien.nienKhoa)
-            && Objects.equals(khoa, sinhVien.khoa);
+    public boolean equals(Object o){
+        return super.equals(o);
     }
 
     @Override
     public String toString() {
-        return super.toString() +
-            ", ngày vào trường = '" + getNgayVaoTruong() + "'" +
-            ", khoa = '" + getKhoa() + "''" +
-            ", niên khoá = '" + getNienKhoa() + "'" +
-            "}";
+        return super.toString() 
+            + ", ngày vào trường = '" + getNgayVaoTruong() + "'";
     }
 }
