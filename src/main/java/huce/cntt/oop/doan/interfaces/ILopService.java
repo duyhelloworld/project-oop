352422b7640b7b52ed0e6c4a4fc2608d1cbf0215
duyhelloworld <codedaovoiduy@gmail.com>
2023-05-24 23:09:18 +0700

    cập nhật vào lớp quản lí

--- src/main/java/huce/cntt/oop/doan/interfaces/ILopService.java
+++ src/main/java/huce/cntt/oop/doan/interfaces/ILopService.java
@@ -3,8 +3,12 @@ package huce.cntt.oop.doan.interfaces;
 import java.sql.SQLException;
 import java.util.List;
 
+import huce.cntt.oop.doan.entities.dto.DTOSinhVien;
+
 public interface ILopService {
     public List<String> layTenCacLopQuanLiTheoKhoa(String tenKhoa);
 
     public void themSinhVienVaoLopQuanLi(Integer mssv, Integer maLopQuanLi) throws SQLException;
+
+    public void capNhatLopQuanLi(DTOSinhVien dtoSinhVien) throws SQLException, IllegalArgumentException;
 }
