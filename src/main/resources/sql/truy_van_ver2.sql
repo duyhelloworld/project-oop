c9bc27dba69de878b168227dd63dd1be2948ae16
duyhelloworld <codedaovoiduy@gmail.com>
2023-05-24 23:11:05 +0700

    tét hàm tí

--- src/main/resources/sql/truy_van_ver2.sql
+++ src/main/resources/sql/truy_van_ver2.sql
@@ -1,10 +1,10 @@
--- Active: 1683203836476@@127.0.0.1@3306@doanoop2
+-- Active: 1683203836476@@127.0.0.1@3306@doanoop
 
 --                          XEM
 
 SELECT * FROM MonHoc INNER JOIN khoa ON khoa.ma_khoa = monhoc.ma_khoa;
 
--- Quản lí danh sách môn + số tín, bắt /tc, môn tiên quyết, khoa
+-- Quản lí danh sách môn  số tín, bắt /tc, môn tiên quyết, khoa
 SELECT 
 ma_monhoc, ten_monhoc, so_tin_chi, bat_buoc, la_tien_quyet_cua, ten_khoa, khoa.ma_khoa, mo_ta
 FROM MonHoc
@@ -26,6 +26,13 @@ ORDER BY sinhvien.mssv;
 -- INSERT INTO sinhvien (ho_ten, gioi_tinh, ngay_sinh, dia_chi_hien_tai, que_quan, email, so_dien_thoai, ngay_vao_truong)
 -- VALUES(?, ?, ?, ?, );
 
+SELECT sinhvien.mssv, ho_ten, gioi_tinh, ngay_sinh, dia_chi_hien_tai, que_quan, email, so_dien_thoai, ngay_vao_truong, ten_lop_quan_li, ten_khoa, khoa.ma_khoa, lopquanli.ma_lop_quan_li
+FROM `SinhVien`
+INNER JOIN lopquanli_sinhvien ON lopquanli_sinhvien.mssv = sinhvien.mssv   
+INNER JOIN lopquanli ON lopquanli.ma_lop_quan_li = lopquanli_sinhvien.ma_lop_quan_li   
+INNER JOIN khoa ON khoa.ma_khoa = lopquanli.ma_khoa  
+ORDER BY sinhvien.mssv;
+
 -- Danh sách giảng viên
 SELECT * FROM giangvien;
 
@@ -42,4 +49,4 @@ ORDER BY monhoc.ma_monhoc;
 SELECT ten_lop_quanli 
 FROM LopQuanLi 
 INNER JOIN khoa ON lopquanli.ma_khoa = khoa.ma_khoa
-WHERE khoa.ten_khoa = "Công Nghệ Thông Tin";
+WHERE khoa.ten_khoa = Công Nghệ Thông Tin;
