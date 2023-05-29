c02094c3edd49a368d860f30c6c8e4ef632edc88
duyhelloworld <codedaovoiduy@gmail.com>
2023-05-29 16:22:27 +0700

    đập làm lại logic tạo bảng sql

--- src/main/resources/sql/tao_bang_ver2.sql
+++ src/main/resources/sql/tao_bang_ver2.sql
@@ -13,33 +13,18 @@ CREATE TABLE MonHoc (
     ten_mon_hoc VARCHAR(20),
     so_tin_chi TINYINT,
     bat_buoc BIT,
-    ma_mon_tien_quyet INT,
-    ma_khoa INT,
+    mon_tien_quyet VARCHAR(20) UNIQUE,
     mo_ta TEXT,
-    UNIQUE (ma_mon_hoc, ten_mon_hoc),
-    Foreign Key (ma_khoa) REFERENCES khoa(ma_khoa)
+    UNIQUE (ma_mon_hoc, ten_mon_hoc)
 );
 
-CREATE TABLE LopMonHoc (
-    ma_lop_mon_hoc INT PRIMARY KEY AUTO_INCREMENT,
-    ten_lop_mon_hoc CHAR(5) UNIQUE,
+CREATE TABLE Khoa_MonHoc (
     ma_mon_hoc INT,
-    Foreign Key (ma_mon_hoc) REFERENCES MonHoc(ma_mon_hoc)
+    ma_khoa INT,
+    PRIMARY KEY(ma_mon_hoc, ma_khoa),
+    Foreign Key (ma_mon_hoc) REFERENCES MonHoc(ma_mon_hoc),
+    Foreign Key (ma_khoa) REFERENCES Khoa(ma_khoa)
 );
-
-CREATE TABLE SinhVien (
-    mssv INT PRIMARY KEY AUTO_INCREMENT,
-    ho_ten VARCHAR(100),
-    gioi_tinh BIT,
-    ngay_sinh DATE,
-    dia_chi_hien_tai VARCHAR(200),
-    que_quan VARCHAR(200), 
-    email VARCHAR(30), 
-    so_dien_thoai CHAR(10),
-    ngay_vao_truong DATE,
-    CONSTRAINT UK_SinhVien UNIQUE(email, so_dien_thoai)
-);
-
 
 CREATE TABLE GiangVien (
     ma_gv INT PRIMARY KEY AUTO_INCREMENT,
@@ -54,44 +39,48 @@ CREATE TABLE GiangVien (
     CONSTRAINT UK_GiangVien UNIQUE(email, so_dien_thoai)
 );
 
-CREATE TABLE LopMonHoc_GiangVien (
+CREATE TABLE LopMonHoc (
+    ma_lop_mon_hoc INT PRIMARY KEY AUTO_INCREMENT,
+    ten_lop_mon_hoc CHAR(5) UNIQUE,
+    ma_mon_hoc INT,
     ma_gv INT,
-    ma_lop_mon_hoc INT,
-    PRIMARY KEY (ma_gv, ma_lop_mon_hoc),
-    Foreign Key (ma_gv) REFERENCES GiangVien(ma_gv),
-    Foreign Key (ma_lop_mon_hoc) REFERENCES LopMonHoc(ma_lop_mon_hoc)
+    Foreign Key (ma_mon_hoc) REFERENCES MonHoc(ma_mon_hoc),
+    Foreign Key (ma_gv) REFERENCES GiangVien(ma_gv)
 );
 
+CREATE TABLE SinhVien (
+    mssv INT PRIMARY KEY AUTO_INCREMENT,
+    ho_ten VARCHAR(100),
+    gioi_tinh BIT,
+    ngay_sinh DATE,
+    dia_chi_thuong_tru VARCHAR(200),
+    que_quan VARCHAR(200), 
+    email VARCHAR(30), 
+    so_dien_thoai CHAR(10),
+    ngay_vao_truong DATE,
+    CONSTRAINT UK_SinhVien UNIQUE(email, so_dien_thoai)
+);
 CREATE TABLE LopQuanLi (
     ma_lop_quan_li INT AUTO_INCREMENT,
     ma_gv INT,
-    ten_lop_quan_li VARCHAR(20),
     ma_khoa INT,
-    constraint uk_lopquanli UNIQUE(ten_lop_quan_li),
+    ten_lop_quan_li VARCHAR(20) UNIQUE,
     PRIMARY KEY(ma_lop_quan_li, ma_gv),
     Foreign Key (ma_khoa) REFERENCES khoa(ma_khoa)
 );
 
-CREATE TABLE LopQuanLi_SinhVien (
-        ma_lop_quan_li INT,
-        mssv INT,
-        PRIMARY KEY(ma_lop_quan_li, mssv),
-        Foreign Key (ma_lop_quan_li) REFERENCES LopQuanLi(ma_lop_quan_li),
-        Foreign Key (mssv) REFERENCES SinhVien(mssv)
-    );
-
-CREATE TABLE NhanVienDaoTao (
-        ma_nvdt INT PRIMARY KEY AUTO_INCREMENT,
-        ho_ten VARCHAR(100),
-        gioi_tinh BIT,
-        ngay_sinh DATE,
-        dia_chi_hien_tai VARCHAR(200),
-        que_quan VARCHAR(200),
-        email VARCHAR(40),
-        so_dien_thoai VARCHAR(10),
-        mat_khau VARCHAR(20),
-        CONSTRAINT UK_GiangVien UNIQUE(email, so_dien_thoai)
-    );
+-- CREATE TABLE NhanVienDaoTao (
+--     ma_nvdt INT PRIMARY KEY AUTO_INCREMENT,
+--     ho_ten VARCHAR(100),
+--     gioi_tinh BIT,
+--     ngay_sinh DATE,
+--     dia_chi_hien_tai VARCHAR(200),
+--     que_quan VARCHAR(200),
+--     email VARCHAR(40),
+--     so_dien_thoai VARCHAR(10),
+--     mat_khau VARCHAR(20),
+--     CONSTRAINT UK_GiangVien UNIQUE(email, so_dien_thoai)
+-- );
 
 CREATE TABLE DiemSinhVien (
     ma_lop_mon_hoc INT,
