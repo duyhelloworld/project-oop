42a257baaacbdaf5dfb2177d799ceac5aa67e3c3
duyhelloworld <codedaovoiduy@gmail.com>
2023-06-01 21:42:20 +0700

    test app

--- src/main/java/huce/cntt/oop/doan/App.java
+++ src/main/java/huce/cntt/oop/doan/App.java
@@ -14,14 +14,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        // Scene QLsinhVien = LoadSinhVien.loadSinhVien();
-        // primaryStage.setScene(QLsinhVien);
+        Scene QLsinhVien = LoadSinhVien.loadSinhVien();
+        primaryStage.setScene(QLsinhVien);
 
         // Scene QLdiemCaNhan = LoadDiemCaNhan.loadDiemCaNhan();
         // primaryStage.setScene(QLdiemCaNhan);
 
-        Scene login = LoadLogin.loadLogin();
-        primaryStage.setScene(login);
+        // Scene login = LoadLogin.loadLogin();
+        // primaryStage.setScene(login);
 
         // Scene trangChu = LoadHome.loadTrangChu();
         // primaryStage.setScene(trangChu);
