3e0700a505f622e04f4e8bf7ddd31d1f031ddb76
duyhelloworld <codedaovoiduy@gmail.com>
2023-06-02 23:54:54 +0700

    đợi merge

--- src/main/java/huce/cntt/oop/doan/loader/LoadDiemHocPhan.java
+++ src/main/java/huce/cntt/oop/doan/loader/LoadDiemHocPhan.java
@@ -3,17 +3,20 @@ package huce.cntt.oop.doan.loader;
 import java.io.IOException;
 
 import huce.cntt.oop.doan.App;
+import huce.cntt.oop.doan.controller.DiemLopHP;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
-public class LoadMonHoc {
-    private static String path = "/view/SinhVien.fxml";
+public class LoadDiemHocPhan {
+    private static String path = "/view/DiemLopHP.fxml";
 
-    public static Scene loadMonHoc() { 
+    public static Scene loadDiemLopHP() { 
         try {
             FXMLLoader loader = new FXMLLoader(App.class.getResource(path));
-            
+            DiemLopHP controller = new DiemLopHP();
+            loader.setController(controller);
+
             // CSS
             
             Pane root = loader.load();
