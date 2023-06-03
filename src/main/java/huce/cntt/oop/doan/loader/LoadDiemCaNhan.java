c5c7fc5aa910ffa3b2a1af2ce1d94db8bfc84a32
duyhelloworld <codedaovoiduy@gmail.com>
2023-06-03 10:17:28 +0700

    thêm nút back

--- src/main/java/huce/cntt/oop/doan/loader/LoadDiemCaNhan.java
+++ src/main/java/huce/cntt/oop/doan/loader/LoadDiemCaNhan.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import huce.cntt.oop.doan.App;
 import huce.cntt.oop.doan.controller.DiemCaNhanController;
+import huce.cntt.oop.doan.entities.GiangVien;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -12,10 +13,10 @@ import javafx.stage.Stage;
 public class LoadDiemCaNhan {
     private static String path = "/view/DiemCaNhan.fxml";
 
-    public static Scene loadDiemCaNhan(Stage stage) { 
+    public static Scene loadDiemCaNhan(Stage stage, GiangVien giangVien) { 
         try {
             FXMLLoader loader = new FXMLLoader(App.class.getResource(path));
-            DiemCaNhanController controller = new DiemCaNhanController(stage);
+            DiemCaNhanController controller = new DiemCaNhanController(stage, giangVien);
             loader.setController(controller);
 
             // CSS
